ad4004f9f4e6a8d2d63b5e1d693fff7cca296977
Quildreen "Sorella" Motta <quildreen@gmail.com>
2013-05-14 23:36:53 -0300

    Defines the minimum API to be implemented.

--- docs/api.hs
+++ docs/api.hs
@@ -2,43 +2,81 @@
 
 -- Base lazy stream
 
-type semigroup S
-  concat a :: (S a) => S a -> S a
+type semigroup s
+  concat A :: (s A) => s A -> s A
 
-type monoid M
-  empty :: () -> M
+type monoid m
+  empty :: () -> m
 
-type functor F
-  map :: (F a) => (a -> b) -> F b
+type functor f
+  map :: (f A) => (A -> B) -> f B
 
-type chain C
-  chain :: (C a) => (a -> C b) -> C b
+type chain c
+  chain :: (c A) => (A -> c B) -> c B
 
-type monad M
-  of :: a -> M a
+type monad m
+  of :: A -> m A
 
-implement Stream a : semigroup a, monoid a, functor a, chain a, monad a
+implement stream A : semigroup A, monoid A, functor A, chain A, monad A
 
 
--- Functions to operate on streams
-replicate :: stream a -> int -> stream a
-iterate :: (a -> b) -> a -> stream b
-cycle :: stream a -> stream a
-interleave :: stream a... -> stream a
-interpose :: a -> stream a -> stream a
+-- Accessing items from a stream
+head :: stream A -> maybe A
+last :: stream A -> maybe A
+tail :: stream A -> stream A
+butLast :: stream A -> stream A
+nth :: int -> stream A -> maybe A
+indexOf :: A -> stream A -> int
 
-concat :: stream a... -> stream a
-distinct :: stream a -> stream a -- | verify an efficient implementation using some sort of set
-filter :: (a -> bool) -> stream a -> stream a
-remove :: (a -> bool) -> stream a -> stream a
 
-reduce :: (b, a -> b) -> strem a -> b
+-- Inspecting properties of a stream
+length :: stream A -> int
+isEmpty :: stream A -> bool
 
-head :: stream a -> Maybe a
-tail :: stream a -> stream a
-take :: int -> stream a -> stream a
-takeWhile :: (a -> bool) -> stream a -> stream a
-drop :: int -> stream a -> stream a
-dropWhile :: (a -> bool) -> stream a -> stream a
-butLast :: stream a -> stream a
-last :: stream a -> Maybe a
+-- Transforming a stream
+map :: (A -> B) -> stream A -> stream B
+reverse :: stream A -> stream A
+interpose :: A -> stream A -> stream A
+interleave :: stream A... -> stream A
+
+-- Reducing streams
+fold :: (B, A -> B) -> B -> stream A -> B
+foldRight :: (B, A -> B) -> B -> stream A -> B
+
+-- Special folds
+concat :: stream A... -> stream A
+mapConcat :: (A -> stream B) -> stream A -> stream B
+and :: stream bool -> bool
+or :: stream bool -> bool
+any :: (A -> bool) -> stream A -> bool
+all :: (A -> bool) -> stream A -> bool
+sum :: stream number -> number
+product :: stream number -> number
+maximum :: stream number -> number
+minimum :: stream number -> number
+
+-- Building streams
+iterate :: (A -> A) -> A -> stream A
+repeat :: A -> stream A
+replicate :: int -> A -> stream A
+cycle :: stream A -> stream A
+
+-- Extracting parts of a stream
+take :: int -> stream A -> stream A
+drop :: int -> stream A -> stream A
+takeWhile :: (A -> bool) -> stream A -> stream A
+dropWhile :: (A -> bool) -> stream A -> stream A
+splitAt :: int -> stream A -> #[stream A, stream A]
+span :: (A -> bool) -> stream A -> #[stream A, stream A]
+break :: (A -> bool) -> stream A -> #[stream A, stream A]
+
+-- Searching things on streams
+contains :: A -> stream A -> bool
+find :: (A -> bool) -> stream A -> maybe A
+filter :: (A -> bool) -> stream A -> stream A
+partition :: (A -> bool) -> stream A -> #[stream A, stream A]
+
+-- Zipping streams
+zip :: stream A... -> [[stream A]]
+zipWith :: (A... -> B) -> stream A... -> [[stream B]]
+unzip :: [[stream A]] -> [[stream A]]
